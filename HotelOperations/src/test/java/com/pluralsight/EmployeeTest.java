443e76f37fb86651e0709f67cbd4a84015c18856
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-07 15:37:27 -0500

    employee test run successful

--- HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
+++ HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
@@ -7,9 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EmployeeTest {
     @Test
     public void punchIn_should_beCorrect() {
-        
+        Employee employee1 = new Employee("Danyelle", 24567, "IT", 30, 40);
+        employee1.punchIn();
+        assertTrue(true);
 
-
+    }
+    @Test
+    public void punchOut_should_beCorrect() {
+        Employee employee1 = new Employee("Danyelle", 24567, "IT", 30, 40);
+        employee1.punchOut();
+        assertTrue(true);
     }
 
 }

4e0d769f766c08d28bb8904b3c0128baad75091e
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-08 11:57:29 -0500

    added notes on how to debug tests at bottom of screen

--- StaticClasses/src/test/java/com/pluralsight/NameFormatterTest.java
+++ StaticClasses/src/test/java/com/pluralsight/NameFormatterTest.java
@@ -54,4 +54,9 @@ class NameFormatterTest {
             assertEquals("Johnson, Mel B", result);
         }
 
+        // sb. append and either adding or taking away the comma is what helped solved this.
+    
+        // rearranging the order of names in the parse to make the appropriate names fall in order /n
+        // between first name and last name
+
 }

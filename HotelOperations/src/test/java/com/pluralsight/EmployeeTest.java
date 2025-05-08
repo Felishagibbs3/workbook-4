19f2742fe6062f0f33a6457d9742d563e9200a06
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-08 09:24:53 -0500

    added comments to employee and room tests

--- HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
+++ HotelOperations/src/test/java/com/pluralsight/EmployeeTest.java
@@ -5,17 +5,25 @@ import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeTest {
+    //punch in should be correct
     @Test
     public void punchIn_should_beCorrect() {
+        //Arrange
         Employee employee1 = new Employee("Danyelle", 24567, "IT", 30, 40);
+        // Act
         employee1.punchIn();
+        //Assert
         assertTrue(true);
 
     }
     @Test
     public void punchOut_should_beCorrect() {
+        // punch out
+        //Arrange
         Employee employee1 = new Employee("Danyelle", 24567, "IT", 30, 40);
+        //Act
         employee1.punchOut();
+        //Assert
         assertTrue(true);
     }
 
--- HotelOperations/src/test/java/com/pluralsight/RoomTest.java
+++ HotelOperations/src/test/java/com/pluralsight/RoomTest.java
@@ -7,31 +7,40 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RoomTest {
     @Test
     public void checkIn_should_beOccupiedAndDirty() {
-        Room room = new Room(2, 124, true, true);
 
+        // check in test
+
+        //Arrange
+        Room room = new Room(2, 124, true, true);
+        //Act
         room.checkIn();
         if (room.isAvailable()) {
             assertTrue(room.isAvailable());
             assertFalse(room.isDirty());
         }
-
+        // Assert
         assertTrue(true);
 
-
+        // Checkout Test below
     }
     @Test
     public void checkOut_should_beTrue() {
+        //Arrange
         Room room = new Room(2, 124, true, false);
+       // Act
         room.checkOut();
+        // Assert
         assertTrue(true);
 
     }
 
     @Test
     public void cleanRoom_should_cleanRoomIfUnavailableAndDirty() {
+        //Arrange
         Room room = new Room(2, 124, true, false);
-
+        // Act
         if (!room.isAvailable() && room.isDirty()){
+        // Assert
             assertTrue(room.cleanRoom());
             assertTrue(true);
         }

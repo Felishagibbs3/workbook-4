f4133b6db56cdcf117cde880398d01af0a78580a
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-13 08:33:30 -0500

    Updated the villian and heros log. Intro screen updated

--- VilliansAndHeros/src/main/java/com/pluralsight/SuperApp.java
+++ VilliansAndHeros/src/main/java/com/pluralsight/SuperApp.java
@@ -1,22 +1,46 @@
 package com.pluralsight;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class SuperApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // make super person with constructor variables from super person class
         SuperVillian mojoJoJo = new SuperVillian("Mojo JoJo", 100, 40);
         SuperHero superMan = new SuperHero("SuperMan", 100, 20);
 
         Random random = new Random();
+        // character's attributes
+        Scanner fighters = new Scanner(System.in);
+        String character1 = "SuperMan";
+        String character2 = "Mojo Jojo";
+        int superManHealth = 100;
+        int mojoJojoHealth = 100;
+
+        // loading screen display
+        System.out.println("=======================================");
+        System.out.println("     BATTLE OF THE UNIVERSE!");
+        System.out.println("========================================");
+        System.out.println();
+        System.out.println(character1 + " " + "VS" + " " + character2);
+        System.out.println();
+        System.out.println(character1 + " - Starting Health: " + superManHealth);
+        System.out.println(character2 + " - Starting Health: " + mojoJojoHealth);
+        System.out.println();
+        System.out.println("Are You Ready for the BATTLE OF THE UNIVERSE?!");
+        System.out.println("Ready Player 1? Hit 'Enter' to FIGHT!");
+        System.out.println("Ready Player 2? Hit 'Enter' to FIGHT!");
+        fighters.nextLine();
+
+
         // > greater than < less than
         // if 1 is less than or equal to 2, they'll attack once and add up to equal 2 then stop
 
-        for (int i = 1; i <= 2; i++) {
-            System.out.println("Mojo JoJo turns and attacks SuperMan");
+        for (int round = 1; mojoJojoHealth > 0; round++) {
+            System.out.println("Mojo JoJo turns and attacks SuperMan with a kick");
             superMan.takeDamage(15);
 
-            // Batman successfully attacked SpiderMan twice, subtracting 15 from SpiderMan
+         break;   // Batman successfully attacked SpiderMan twice, subtracting 15 from SpiderMan
         }
 
 
@@ -40,6 +64,24 @@ public class SuperApp {
                 break;
             }
             System.out.println(mojoJoJo.getStatus());
+
+            for (int i4 = 5; i4 >4;) {
+                superMan.takeDamage(40);
+                System.out.println("Superman gets blasted with a ray blast");
+                break;
+            }
+            System.out.println(superMan.getStatus());
+            System.out.println("Superman stands up..");
+
+            for (int i5 = 100; i5 >25;){
+                mojoJoJo.takeDamage(25);
+                System.out.println("Superman FINISHES Mojo Jo with a Solar Flare");
+                break;
+            }
+            System.out.println(mojoJoJo.getStatus());
+            System.out.println("=======================================");
+            System.out.println("     SUPERMAN HAS SAVED US!");
+            System.out.println("=======================================");
         }
         // check status after taking damage
 
--- VilliansAndHeros/src/main/java/com/pluralsight/SuperVillian.java
+++ VilliansAndHeros/src/main/java/com/pluralsight/SuperVillian.java
@@ -21,3 +21,30 @@ public class SuperVillian extends SuperPerson{
         }
     }
 }
+
+
+
+// Create a hero object with a name, health, and powerLevel
+// Create a villain object with a name, health, and evilness
+
+// Display an introduction message
+// Show the starting status of both characters
+
+// Loop while both the hero and villain are still alive
+// Indicate the hero's turn
+// Have the hero fight the villain
+// Show the villain's updated status
+
+// If the villain is no longer alive, break the loop
+
+// Indicate the villain's turn
+// Have the villain fight the hero
+// Show the hero's updated status
+// End of loop
+
+// Display a message that the battle is over
+
+// If the hero is still alive
+// Announce the hero as the winner
+// Otherwise
+// Announce the villain as the winner
